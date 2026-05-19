dde2a30f2d4abb3d0ae00c98f2a32f695136a135
nylzy <133775866+nylzy@users.noreply.github.com>
2026-05-19 19:00:11 +0800

    Protocols completed, restructured files

---
+++ devices.py
@@ -0,0 +1,44 @@
+import math
+from protocol import UDPSegment
+
+##### TRANSPORT LAYER #####
+class TransportLayer:
+
+    def __init__(self):
+        self.seq = 0
+        
+        
+    def _receiveAbove(self, size:int):
+        print(f"Layer 4: Data received from the Application Layer, Data size = {size}")
+        if size > 500:
+            numSegments = math.ceil(size/500)
+            print(f"Data size too large for a single UDP segment, dividing data into {numSegments} segments")
+        else:
+            newSegment = self.TransportSegment(size, 0)
+    
+    def _receiveBelow(self, packet):
+        segment = packet.data
+        print("Layer 4: Segment received from the Network Layer")
+        if segment.seq == self.seq:
+            if segment.type == 0:
+                self._sendAbove(segment)
+                newSegment = self.TransportSegment(0, 1)
+                self._sendBelow(newSegment)
+            else:
+                print(f"Layer 4: ACK received, seq = {segment.seq}")
+            self.seq = 1 - self.seq
+                
+        else:
+            print('error lol')
+    
+    def _sendBelow(self, segment):
+        print(f"Layer 4: Sending segment to Network Layer ({ "ACK" if segment.type else "DATA"}, size = {segment.size})")
+    
+    def _sendAbove(self, segment):
+        print(f"DATA segment delivered to Application Layer, Data size = {segment.data}")
+    
+    def _verifyChecksum(self):
+        print('Layer 4: Checksum Verified')
+        
+    def _computeChecksum():
+        print('Layer 4: Checksum Computed')
--- protocol.py
+++ protocol.py
@@ -1,51 +1,26 @@
-import math
 
-class TransportLayer:
-    class TransportSegment:
-        def __init__(self, size:int, type:int):
-            self.sourcePort = 5000
-            self.destPort = 5001
-            self.length = 10 + size
-            self.checksum = 0
-            self.type = 0
-            self.seq = 0
-            self.data = size
-    
-    def __init__(self):
-        self.seq = 0
-        
-        
-    def _receiveAbove(self, size:int):
-        print(f"Layer 4: Data received from the Application Layer, Data size = {size}")
-        if size > 500:
-            numSegments = math.ceil(size/500)
-            print(f"Data size too large for a single UDP segment, dividing data into {numSegments} segments")
-        else:
-            newSegment = self.TransportSegment(size, 0)
-    
-    def _receiveBelow(self, packet):
-        segment = packet.data
-        print("Layer 4: Segment received from the Network Layer")
-        if segment.seq == self.seq:
-            if segment.type == 0:
-                self._sendAbove(segment)
-                newSegment = self.TransportSegment(0, 1)
-                self._sendBelow(newSegment)
-            else:
-                print(f"Layer 4: ACK received, seq = {segment.seq}")
-            self.seq = 1 - self.seq
-                
-        else:
-            print('error lol')
-    
-    def _sendBelow(self, segment):
-        print(f"Layer 4: Sending segment to Network Layer ({ "ACK" if segment.type else "DATA"}, size = {segment.size})")
-    
-    def _sendAbove(self, segment):
-        print(f"DATA segment delivered to Application Layer, Data size = {segment.data}")
-    
-    def _verifyChecksum():
-        print('Layer 4: Checksum Verified')
-        
-    def _computeChecksum():
-        print('Layer 4: Checksum Computed')
+
+
+
+##### HELPER FUNCTIONS #####
+
+# Checksum the data for errors
+def compute_checksum(data: bytes) -> int:
+    checksum = 0
+
+    for byte in data:
+        checksum += byte
+
+    return checksum % 65536 # Due to 65526 ciombinations in a 16-bit value 
+
+
+class UDPSegment:
+        def __init__(self, src_port, dst_port, seg_type, seq, data=b""):
+            self.src_port = src_port
+            self.dst_port = dst_port 
+            self.length = len(data) + 10
+            self.checksum = compute_checksum(data)
+            self.type = seg_type
+            self.seq = seq
+            self.data = data
+
