1801a3a371b3f1fc917ad565d089b22c4c1868ac
nylzy <133775866+nylzy@users.noreply.github.com>
2026-05-20 15:34:29 +0800

    Removed transport layer fromd devices.py

--- devices.py
+++ devices.py
@@ -1,44 +1,22 @@
-import math
-from protocol import UDPSegment
+from protocol import TransportLayer, NetworkLayer, DataLinkLayer
 
-##### TRANSPORT LAYER #####
-class TransportLayer:
+class Host:
+    def __init__(self, name, ip, mac, routing_table, arp_table):
+        self.name = name
+        self.ip = ip
+        self.mac = mac
+        self.routing_table = routing_table
+        self.arp_table = arp_table
+        self.transport = TransportLayer(self)
+        self.network = NetworkLayer(self)
+        self.datalink = DataLinkLayer(self)
 
-    def __init__(self):
-        self.seq = 0
-        
-        
-    def _receiveAbove(self, size:int):
-        print(f"Layer 4: Data received from the Application Layer, Data size = {size}")
-        if size > 500:
-            numSegments = math.ceil(size/500)
-            print(f"Data size too large for a single UDP segment, dividing data into {numSegments} segments")
-        else:
-            newSegment = self.TransportSegment(size, 0)
-    
-    def _receiveBelow(self, packet):
-        segment = packet.data
-        print("Layer 4: Segment received from the Network Layer")
-        if segment.seq == self.seq:
-            if segment.type == 0:
-                self._sendAbove(segment)
-                newSegment = self.TransportSegment(0, 1)
-                self._sendBelow(newSegment)
-            else:
-                print(f"Layer 4: ACK received, seq = {segment.seq}")
-            self.seq = 1 - self.seq
-                
-        else:
-            print('error lol')
-    
-    def _sendBelow(self, segment):
-        print(f"Layer 4: Sending segment to Network Layer ({ "ACK" if segment.type else "DATA"}, size = {segment.size})")
-    
-    def _sendAbove(self, segment):
-        print(f"DATA segment delivered to Application Layer, Data size = {segment.data}")
-    
-    def _verifyChecksum(self):
-        print('Layer 4: Checksum Verified')
-        
-    def _computeChecksum():
-        print('Layer 4: Checksum Computed')
+class Router:
+    def __init__(self, name, routing_table, arp_table, iface_mac, iface_ip):
+        self.name = name
+        self.routing_table = routing_table
+        self.arp_table = arp_table
+        self.iface_mac = iface_mac
+        self.iface_ip = iface_ip
+        self.network = NetworkLayer(self)
+        self.datalink = DataLinkLayer(self)
